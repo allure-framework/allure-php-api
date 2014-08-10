a6a6cd8748d20ace3683c8e0a74d55b54a253288
Ivan Krutov <vania-pooh@vania-pooh.com>
2014-08-10 23:22:36 +0400

    Attempt to ignore annotations

--- test/Yandex/Allure/Adapter/Annotation/AnnotationProviderTest.php
+++ test/Yandex/Allure/Adapter/Annotation/AnnotationProviderTest.php
@@ -4,6 +4,11 @@ namespace Yandex\Allure\Adapter\Annotation;
 
 use Doctrine\Common\Annotations\Annotation\Required;
 
+const TYPE_CLASS = 'class';
+const TYPE_METHOD = 'method';
+const ANNOTATION_NAME = 'TestAnnotation';
+const METHOD_NAME = 'methodWithAnnotations';
+
 class AnnotationProviderTest extends \PHPUnit_Framework_TestCase {
     
     public function testGetClassAnnotations()
@@ -13,18 +18,27 @@ class AnnotationProviderTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(sizeof($annotations) === 1);
         $annotation = array_pop($annotations);
         $this->assertTrue($annotation instanceof TestAnnotation);
-        $this->assertEquals('class', $annotation->value);
+        $this->assertEquals(TYPE_CLASS, $annotation->value);
     }
     
     public function testGetMethodAnnotations()
     {
         $instance = new ClassWithAnnotations();
-        $annotations = AnnotationProvider::getMethodAnnotations($instance, 'methodWithAnnotations');
+        $annotations = AnnotationProvider::getMethodAnnotations($instance, METHOD_NAME);
         $this->assertTrue(sizeof($annotations) === 1);
         $annotation = array_pop($annotations);
         $this->assertTrue($annotation instanceof TestAnnotation);
-        $this->assertEquals('method', $annotation->value);
+        $this->assertEquals(TYPE_METHOD, $annotation->value);
     }
+
+    public function testIgnoreAnnotations()
+    {
+        $instance = new ClassWithAnnotations();
+        AnnotationProvider::addIgnoredAnnotations([ANNOTATION_NAME]);
+        $this->assertEmpty(AnnotationProvider::getClassAnnotations($instance));
+        $this->assertEmpty(AnnotationProvider::getMethodAnnotations($instance, METHOD_NAME));
+    }
+
 }
 
 /**
